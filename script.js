b92f60d27175f11f561aa3006045939f431b459b
mybookshop1 <davlatraupov@gmail.com>
2020-07-19 18:48:08 +0500

    gft

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ $('.menu-btn').on('click', function(a) {
   $('.menu').toggleClass('menu_active');
   $('.content').toggleClass('content_active');
 });
+
+$('#grgf').on('click', function(a) {
+  a.preventDefault();
+     $('.districtts').removeClass('districtts_active');
+     $('.districtgr').addClass('districtgr_active');
+    $('.districtrg').removeClass('districtrg_active');
+    });
+
+$('#rgrbrn').on('click', function(a) {
+  a.preventDefault();
+     $('.districtts').removeClass('districtts_active');
+     $('.districtgr').removeClass('districtgr_active');
+    $('.districtrg').addClass('districtrg_active');
+    });
+
+$('#tptc').on('click', function(a) {
+  a.preventDefault();
+     $('.districtts').addClass('districtts_active');
+     $('.districtgr').removeClass('districtgr_active');
+    $('.districtrg').removeClass('districtrg_active');
+    });
